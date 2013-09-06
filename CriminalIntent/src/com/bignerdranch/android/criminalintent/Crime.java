0f1d67d0aacdab4fe2d2a7ef4fba927392db229d
Jason Atwood <jatwood@bignerdranch.com>
2013-09-06 16:10:29 -0400

    CriminalIntent: - finishes Ch7 - finishes Ch8

---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+	private UUID mID;
+	private String mTitle;
+	private Date mDate; // date crime occurred
+	private boolean mSolved; // 
+	
+	public Crime(){ // constructor
+		mID = UUID.randomUUID(); // generate random (unique?) ID
+		mDate = new Date(); // defaults to current date
+	}
+
+	// getters and setters
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public UUID getID() {
+		return mID;
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean isSolved) {
+		mSolved = isSolved;
+	}
+	
+}
---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,80 @@
+package com.bignerdranch.android.criminalintent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+//REMEMBER: this is a controller: controlling the view of a single crime!
+public class CrimeFragment extends Fragment {
+
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState); // call super method
+		mCrime = new Crime();				// instantiate new Crime
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+							 ViewGroup parent, 
+							 Bundle savedInstanceState){
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false); 	// inflate view
+		
+		
+		// handle mTitleField
+		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() 
+			{ // begin anonymous inner class, with three class methods
+				public void onTextChanged(CharSequence c,
+											int start,
+											int before,
+											int count){
+					mCrime.setTitle(c.toString());			// convert charSeq to string; set crime's title to string
+				}
+				
+				public void beforeTextChanged(CharSequence c,
+												int start,
+												int before,
+												int count){
+					// purposely left blank
+				}
+	
+				public void afterTextChanged(Editable c){
+					// purposely left blank
+				}
+			} // ends anonymous inner class
+		);
+		
+		// handle mDateButton
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+		
+		// handle mSolvedCheckBox;
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener()
+			{ // anonymous inner class for listener; define single method
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+					// set the crime's solved variable
+					mCrime.setSolved(isChecked);
+				}
+			} // end anonymous inner class for listener
+		);
+		
+		return v;															// return view
+	}
+
+}
