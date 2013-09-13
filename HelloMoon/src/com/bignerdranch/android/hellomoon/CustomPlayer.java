f9147997ddbe0c4c5aba0771524d4af4c88727d5
Jason Atwood <jatwood@bignerdranch.com>
2013-09-13 18:09:19 -0400

    HelloMoon: - starts ch13 challenge #2 - contains bug: http://forums.bignerdranch.com/viewtopic.php?f=410&t=7189 - simple hack in place to work around

--- HelloMoon/src/com/bignerdranch/android/hellomoon/CustomPlayer.java
+++ HelloMoon/src/com/bignerdranch/android/hellomoon/CustomPlayer.java
@@ -1,26 +1,41 @@
 package com.bignerdranch.android.hellomoon;
 
+import android.R.bool;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.util.Log;
+import android.view.SurfaceHolder;
 
-public class AudioPlayer {
+public class CustomPlayer {
 	
 	// NOTE on pausing: I don't want to pause the MediaPlayer indefinitely.
 	// So, I "pause" the AudioPlayer by pausing MediaPlayer, getting the current position of the MediaPlayer, 
-	// save that position to AudioPlayer.mPosition. Then on "play" I either start or resume from mPosition.
+	// save that position to AudioPlayer.mPosition, then release it. Then on "play" I just resume from mPosition, even if mPosition = 0 (beginning)
 	
 	// TODO: eventually I will have to preserve mPosition across instances (i.e during rotation, for now don't worry about it)
 	
-	private int mPosition  = 0; // instantiate to zero (the beginning)
+	private int mPosition  = 0; 					// instantiate to zero (the beginning)
 	private MediaPlayer mPlayer;
+	private int mResID; 							// resource ID for media to be played
 	public static final String TAG = "helloMoon";
 	
+	// constructor
+	public CustomPlayer(int resID){
+		mResID = resID; 
+	}
+	
 	public void play(Context context){
-		Log.d(TAG, "inside AudioPlayer.play()");
+		// begin by releasing
 		release();
 		
-		mPlayer = MediaPlayer.create(context, R.raw.one_small_step);
+		// create player
+		mPlayer = MediaPlayer.create(context, mResID);
+		
+		// confirm create
+		if (mPlayer == null){
+			Log.w(TAG, "player not created");
+			return;
+		}
 		
 		// setup  listeners
 		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() 
@@ -36,7 +51,6 @@ public class AudioPlayer {
 			{
 				@Override
 				public void onSeekComplete(MediaPlayer player) {
-					Log.d(TAG, "actually about to start");
 					player.start(); // start
 				}
 			}
@@ -47,13 +61,11 @@ public class AudioPlayer {
 	}
 	
 	public void stop(){
-		Log.d(TAG, "inside AudioPlayer.stop()");
 		release();	 	// release
 		mPosition = 0;  // reset mPosition
 	}
 	
 	public void pause(){
-		Log.d(TAG, "inside AudioPlayer.pause()");
 		if(mPlayer != null){							// null check
 			mPlayer.pause();							// pause
 			mPosition = mPlayer.getCurrentPosition(); 	// get current position
@@ -62,10 +74,16 @@ public class AudioPlayer {
 	}
 	
 	private void release(){
-		Log.d(TAG, "inside AudioPlayer.release()");
 		if(mPlayer != null){							// null check
 			mPlayer.release();							// release it
 			mPlayer = null;								// and make null
+		}
+	}
+
+	// only needed for video
+	public void setCustomDisplay(SurfaceHolder surfaceHolder){
+		if (mPlayer != null){ 						// null check
+			mPlayer.setDisplay(surfaceHolder);		// set display via surface holder
 		}
 	}
 }
--- HelloMoon/src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -11,49 +12,100 @@ import android.widget.Button;
 
 public class HelloMoonFragment extends Fragment {
 
-	private Button mPlayButton;
-	private Button mPauseButton;
-	private Button mStopButton;
-	private AudioPlayer mPlayer = new AudioPlayer();
+	private SurfaceView mSurfaceView;
+	
+	private Button mPlayVideoButton;
+	private Button mPauseVideoButton;
+	private Button mStopVideoButton;
+	private Button mPlayAudioButton;
+	private Button mPauseAudioButton;
+	private Button mStopAudioButton;
+	
+	private CustomPlayer mVideoPlayer = new CustomPlayer(R.raw.apollo_17_stroll);
+	private CustomPlayer mAudioPlayer = new CustomPlayer(R.raw.one_small_step);
 	public static final String TAG = "helloMoon";
+	
 	
 	public View onCreateView(LayoutInflater inflater,
 								ViewGroup parent,
 								Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 		
-		// handle play button
-		mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
-		mPlayButton.setOnClickListener(new View.OnClickListener() 
-			{
+		// ================ video surface view ===============
+		mSurfaceView = (SurfaceView)v.findViewById(R.id.helloMoon_videoSurfaceView);
+		if (mSurfaceView == null){
+			Log.w(TAG, "I have no surface view");
+		}
+		mVideoPlayer.setCustomDisplay(mSurfaceView.getHolder()); // set videoplayer's surface
+		
+		// =============== video buttons ====================
+		// handle play button 
+		mPlayVideoButton = (Button)v.findViewById(R.id.hellomoon_playVideoButton);
+		mPlayVideoButton.setOnClickListener(new View.OnClickListener() 	{
 				@Override
 				public void onClick(View view) {
-					Log.d(TAG, "about to call play");
-					mPlayer.play(getActivity());
+					mVideoPlayer.play(getActivity());
 				}
 			}
 		);
 		
 		// handle pause button
-		mPauseButton = (Button)v.findViewById(R.id.hellomoon_pauseButton);
-		mPauseButton.setOnClickListener(new OnClickListener() 
+		mPauseVideoButton = (Button)v.findViewById(R.id.hellomoon_pauseVideoButton);
+		mPauseVideoButton.setOnClickListener(new OnClickListener() 
 			{
 				@Override
 				public void onClick(View v) {
-					Log.d(TAG, "about to call pause");
-					mPlayer.pause();
+					mVideoPlayer.pause();
 				}
 			}
 		);
 		
 		// handle stop button
-		mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
-		mStopButton.setOnClickListener(new View.OnClickListener() 
+		mStopVideoButton = (Button)v.findViewById(R.id.hellomoon_stopVideoButton);
+		mStopVideoButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View view) {
+				Log.d(TAG, "about to call stop");
+				mVideoPlayer.stop();
+			}
+		}
+	);
+				
+				
+		// =============== audio buttons ====================
+		// handle play button
+		mPlayAudioButton = (Button)v.findViewById(R.id.hellomoon_playAudioButton);
+		mPlayAudioButton.setOnClickListener(new View.OnClickListener() 
+			{
+				@Override
+				public void onClick(View view) {
+					Log.d(TAG, "about to call play");
+					mAudioPlayer.play(getActivity());
+				}
+			}
+		);
+		
+		// handle pause button
+		mPauseAudioButton = (Button)v.findViewById(R.id.hellomoon_pauseAudioButton);
+		mPauseAudioButton.setOnClickListener(new OnClickListener() 
+			{
+				@Override
+				public void onClick(View v) {
+					Log.d(TAG, "about to call pause");
+					mAudioPlayer.pause();
+				}
+			}
+		);
+		
+		// handle stop button
+		mStopAudioButton = (Button)v.findViewById(R.id.hellomoon_stopAudioButton);
+		mStopAudioButton.setOnClickListener(new View.OnClickListener() 
 			{
 				@Override
 				public void onClick(View view) {
 					Log.d(TAG, "about to call stop");
-					mPlayer.stop();
+					mAudioPlayer.stop();
 				}
 			}
 		);
@@ -64,6 +116,6 @@ public class HelloMoonFragment extends Fragment {
 	@Override
 	public void onDestroy(){
 		super.onDestroy(); // call super class
-		mPlayer.stop();
+		mAudioPlayer.stop();
 	}
 }
