0c8372ceb9720b50b1692e49843b329cfdbd18d2
Jason Atwood <jatwood@bignerdranch.com>
2014-08-01 10:40:47 -0700

    ch33 run tracker initial plumbing

---
+++ RunTracker/app/src/main/java/com/bignerdranch/android/runtracker/RunFragment.java
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.runtracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RunFragment extends Fragment {
+
+    private Button mStartButton, mStopButton;
+    private TextView mStartedTextView, mLatitudeTextView, mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+
+    public static RunFragment newInstance() {
+        return new RunFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_run, container, false);
+
+        mStartedTextView = (TextView) view.findViewById(R.id.run_startedTextView);
+        mLatitudeTextView = (TextView) view.findViewById(R.id.run_latitudeTextView);
+        mLongitudeTextView = (TextView) view.findViewById(R.id.run_longitudeTextView);
+        mAltitudeTextView = (TextView) view.findViewById(R.id.run_altitudeTextView);
+        mDurationTextView = (TextView) view.findViewById(R.id.run_durationTextView);
+
+        mStartButton = (Button) view.findViewById(R.id.run_startButton);
+        mStopButton = (Button) view.findViewById(R.id.run_stopButton);
+
+        return view;
+    }
+}
