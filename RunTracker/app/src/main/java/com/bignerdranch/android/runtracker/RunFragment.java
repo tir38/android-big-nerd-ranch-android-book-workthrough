99a0329aabcd565886cefa66645483f876a213f0
Jason Atwood <jatwood@bignerdranch.com>
2014-10-09 21:21:32 -0400

    finishes ch33

--- RunTracker/app/src/main/java/com/bignerdranch/android/runtracker/RunFragment.java
+++ RunTracker/app/src/main/java/com/bignerdranch/android/runtracker/RunFragment.java
@@ -1,5 +1,9 @@
 package com.bignerdranch.android.runtracker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,11 +11,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RunFragment extends Fragment {
 
     private Button mStartButton, mStopButton;
     private TextView mStartedTextView, mLatitudeTextView, mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+
+    private RunManager mRunManager;
+    private Run mRun;
+    private Location mLastLocation;
+
+    private BroadcastReceiver mLocationReceiver = new LocationReceiver() {
+        @Override
+        protected void onLocationReceived(Context context, Location location) {
+            mLastLocation = location;
+            if (isVisible()) {
+                updateUI();
+            }
+        }
+
+        @Override
+        protected void onProviderEnabledChanged(boolean enabled) {
+            int toastText = enabled ? R.string.gps_enabled : R.string.gps_disabled;
+            Toast.makeText(getActivity(), toastText, Toast.LENGTH_LONG).show();
+        }
+    };
 
     public static RunFragment newInstance() {
         return new RunFragment();
@@ -21,6 +46,7 @@ public class RunFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        mRunManager = RunManager.get(getActivity());
     }
 
     @Override
@@ -33,9 +59,58 @@ public class RunFragment extends Fragment {
         mAltitudeTextView = (TextView) view.findViewById(R.id.run_altitudeTextView);
         mDurationTextView = (TextView) view.findViewById(R.id.run_durationTextView);
 
-        mStartButton = (Button) view.findViewById(R.id.run_startButton);
         mStopButton = (Button) view.findViewById(R.id.run_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRunManager.stopLocationUpdates();
+                updateUI();
+            }
+        });
 
+        mStartButton = (Button) view.findViewById(R.id.run_startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRunManager.startLocationUpdates();
+                mRun = new Run();
+                updateUI();
+            }
+        });
+
+        updateUI();
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().registerReceiver(mLocationReceiver, new IntentFilter(RunManager.ACTION_LOCATION));
+    }
+
+    @Override
+    public void onStop() {
+        getActivity().unregisterReceiver(mLocationReceiver); // we have to unregister before we call to super
+        super.onStop();
+    }
+
+    private void updateUI() {
+        boolean started = mRunManager.isTrackingRun();
+
+        if (mRun != null) {
+            mStartedTextView.setText(mRun.getStartDate().toString());
+        }
+
+        int durationSeconds = 0;
+        if (mRun != null && mLastLocation != null) {
+            durationSeconds = mRun.getDurationSeconds(mLastLocation.getTime());
+            mLatitudeTextView.setText(Double.toString(mLastLocation.getLatitude()));
+            mLongitudeTextView.setText(Double.toString(mLastLocation.getLongitude()));
+            mAltitudeTextView.setText(Double.toString(mLastLocation.getAltitude()));
+        }
+        mDurationTextView.setText(Run.formatDuration(durationSeconds));
+
+        mStartButton.setEnabled(!started);
+        mStopButton.setEnabled(started);
     }
 }
